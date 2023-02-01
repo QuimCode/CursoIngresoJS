3248f7725c4b3d6727fa748d82530c62dac18f21
Quimey Fontan <quimafoncode@gmail.com>
2023-02-01 11:25:11 -0300

    v2.0

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -1,18 +1,21 @@
-/*
+/*Quimey Alejo Fontan
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar() {
-
 	let Num1;
 	let Num2;
 	let Resultado;
-	// falta los documents //
+	let mensaje;
+
+	Num1 = document.getElementById("txtIdNumeroUno").value;
+	Num2 = document.getElementById("txtIdNumeroDos").value;
 	Num1 = parseInt(Num1);
 	Num2 = parseInt(Num2);
 
 	Resultado = Num1 + Num2;
+	mensaje = "La suma es " + Resultado;
 
-	alert("El Resultado es ... " + Resultado);
+	alert(mensaje);
 }
 
