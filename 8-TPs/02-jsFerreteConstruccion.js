8bd03c72cee65184e28f86964d4b9f1e2c844101
Quimey Fontan <quimafoncode@gmail.com>
2023-02-03 18:12:51 -0300

    v2.6

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -7,9 +7,10 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 function Rectangulo() {
     let largoTerreno;
     let anchoTerreno;
-    let sumaLarAnc;
     let hilosAlambre = 3;
     let ladosTerreno = 2;
+    let sumaLarAnc;
+
 
     largoTerreno = parseFloat(document.getElementById("txtIdLargo").value);
     anchoTerreno = parseFloat(document.getElementById("txtIdAncho").value);
@@ -17,19 +18,55 @@ function Rectangulo() {
 
     sumaLarAnc = (largoTerreno + anchoTerreno) * ladosTerreno * hilosAlambre;
 
+    alert("La Cantidad de alambre a comprar es ... " + sumaLarAnc);
+
 }
+
+
 function Circulo() {
+    let largoTerreno;
+    let anchoTerreno;
     let radioTerreno;
+    let PI = 3.14;
     let hilosAlambre = 3;
+    let radioCirculo;
+    let sumaLarAncRad;
 
+    largoTerreno = parseFloat(document.getElementById("txtIdRadio").value);
+    anchoTerreno = parseFloat(document.getElementById("txtIdAncho").value);
+    radioTerreno = parseFloat(document.getElementById("txtIdRadio").value);
 
-    radioTerreno = parseInt(document.getElementById("txtIdRadio").value);
+    radioCirculo = 2 * radioTerreno;
+    radioCirculo = radioCirculo * PI;
+    sumaLarAncRad = (largoTerreno + anchoTerreno + radioCirculo) * hilosAlambre;
+
+    alert("La Cantidad de alambre a comprar es ... " + sumaLarAncRad);
 
 }
+
+
 function Materiales() {
 
+    let metroLargo;
+    let metroAncho;
+    let metroCuadrado;
+    let bolsaCemento = 2;
+    let bolsaCal = 3;
+    let cantidadDeBolsaCal;
+    let cantidadDeBolsaCem;
+
+    metroLargo = parseFloat(document.getElementById("txtIdLargo").value);
+    metroAncho = parseFloat(document.getElementById("txtIdAncho").value);
+
+    metroCuadrado = metroAncho * metroLargo;
+    cantidadDeBolsaCem = bolsaCemento * metroCuadrado;
+    cantidadDeBolsaCal = bolsaCal * metroCuadrado;
+
+    alert("La cantidad de bolsas de Cemento a usar son ... " + cantidadDeBolsaCem);
+    alert("La cantidad de bolsas de Cal a usar son ... " + cantidadDeBolsaCal);
 }
+
 
 // Perimetro de un circulo es 2 * Pi (3,14) * R (radio)
 // Para sacar el Area necesitada para calcular la cantidad de bolsas, se usa el calculo de Ancho * Largo (ingresado por el usuario),
-//  para sacar las bolsas de cal o cemento de multiplican por 2 en base a los metros cudrados dados por el largo * ancho.
+// para sacar las bolsas de cal o cemento de multiplican por 2 en base a los metros cudrados dados por el largo * ancho.
--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -2,12 +2,28 @@
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
-function FahrenheitCentigrados () 
-{
-	
+function FahrenheitCentigrados() {
+    let gradoFahrenheit
+    let gradoCentigrado
+
+    gradoFahrenheit = parseFloat(document.getElementById("txtIdTemperatura").value);
+
+    gradoCentigrado = (gradoFahrenheit - 32) * 5 / 9;
+
+    alert("Los " + gradoFahrenheit + " Fahrenheit ingresados son " + gradoCentigrado + " Centigrados.");
+
 }
 
-function CentigradosFahrenheit () 
-{
-	
+
+function CentigradosFahrenheit() {
+    let gradoCentigrado
+    let gradoFahrenheit
+
+    gradoCentigrado = parseFloat(document.getElementById("txtIdTemperatura").value);
+
+    gradoFahrenheit = (gradoCentigrado * 9 / 5) + 32;
+
+    alert("Los " + gradoCentigrado + " Centigrados ingresados son " + gradoFahrenheit + " Fahrenheit.");
+
 }
+
