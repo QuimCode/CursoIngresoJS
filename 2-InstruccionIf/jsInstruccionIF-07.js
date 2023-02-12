39d35bb04dc7fadf253e73c99a230234006882e9
Quimey Fontan <quimafoncode@gmail.com>
2023-02-12 13:18:01 -0300

    v2.7

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,18 @@
-function mostrar()
-{
+function mostrar() {
 	//tomo la edad  
-	alert("ok");
-	
+	let edadIngresada;
+	let estadoCivil;
+	let mensaje;
+
+	edadIngresada = parseInt(document.getElementById("txtIdEdad").value);
+	estadoCivil = document.getElementById("estadoCivil").value;
+
+	if (edadIngresada < 18 && estadoCivil != "Soltero") {
+		mensaje = "Es muy pequeño para NO ser soltero."
+		alert(mensaje)
+	}
+
+
 
 
 }//FIN DE LA FUNCIÓN
