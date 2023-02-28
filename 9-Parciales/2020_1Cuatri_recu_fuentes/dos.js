0b481c5058356ee80eb0869efe11095aa61dd1e4
Quimey Fontan <quimafoncode@gmail.com>
2023-02-28 01:51:26 -0300

    v2.5 Final

--- 9-Parciales/2020_1Cuatri_recu_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_recu_fuentes/dos.js
@@ -1,3 +1,4 @@
+// Quimey Alejo Fontan 
 // Realizar el algoritmo que permita ingresar los datos de una compra de productos de la construccion, hasta que el cliente quiera:
 // Tipo validad("arena";"cal";"cemento")
 // Cantidad de bolsas,
@@ -11,56 +12,151 @@
 // f) El tipo mas caro
 
 function mostrar() {
-  let banderaInicio;
-  let banderaInicioCantidad;
+  let respuesta;
+  let banderaPrecio;
+  let banderaCuentaPagar;
 
-  let tipoPro;
-  let cantidadPro;
-  let precioBolsa;
+  let tipoBolsa;
+  let precio;
+  let cantidad;
 
-  let cantidadBolsa
+  let cantBolsaArena;
+  let cantBolsaCal;
+  let cantBolsaCemento;
 
-  banderaInicio == true;
-  banderaInicioCantidad == true;
+  let precionMaximo;
+  let precioMinimo;
 
-  cantidadBolsa = 0;
+  let precioArena;
+  let precioCemento;
+  let precioCal;
 
-  while (banderaInicio == true) {
+  let primerDescuento;
+  let segundoDescuento;
 
-    tipoPro = prompt("Ingrese el tipo de producto a comprar 'arena'. 'cal' y 'cemento'.")
-    if (!isNaN(tipoPro) && tipoPro != 'arena' && tipoPro != 'cal' && tipoPro != 'cemento') {
-      tipoPro = prompt("Incorrecto, ingrese el tipo de producto a comprar 'arena'. 'cal' y 'cemento'.")
+  let precioFinal;
+  let precioConDescuento;
+
+  banderaInicializacion = true;
+  banderaPrecio;
+  banderaCuentaPagar = true;
+
+  precioMinimo = 0;
+  precionMaximo = 1000;
+
+  cantBolsaArena = 0;
+  cantBolsaCal = 0;
+  cantBolsaCemento = 0;
+
+  primerDescuento = 0.15;
+  segundoDescuento = 0.25;
+
+  while (banderaInicializacion == true) {
+
+    /* TIPO INGRESADO-----------------------------------------------------------------------*/
+    tipoBolsa = prompt("Ingrese el tipo de productor a comprar con los siguientes valores 'A'-Arena, 'B'-Cal, 'C'-Cemento.")
+    while (tipoBolsa !== 'A' && tipoBolsa !== 'B' && tipoBolsa !== 'C') {
+      tipoBolsa = prompt("Por favor, ingrese un tipo de valor asignado 'A', 'B' o 'C'.");
     }
 
-    cantidadPro = parseInt(prompt("Ingrese el tipo de producto a comprar 'arena'. 'cal' y 'cemento'."))
-    if (isNaN(cantidadPro)) {
-      cantidadPro = parseInt(prompt("Incorrecto, ingrese el precio del producto en gondola."))
+    alert("Genial, prosiga oprimiendo 'ACEPTAR' e ingresando el precio.");
+
+    /* PRECIO INGRESADO-----------------------------------------------------------------------*/
+    precio = parseFloat(prompt("Ingrese el precio asignado en gondola del producto que desea llevar."))
+    while (isNaN(precio) || precio <= precioMinimo || precio > precionMaximo) {
+      precio = parseFloat(prompt("Por favor, ingrese un tipo de valor numérico que sea mayor a 0 y menor o igual a 1000."));
     }
 
-    precioBolsa = parseInt(prompt("Ingrese el tipo de producto a comprar 'arena'. 'cal' y 'cemento'."))
-    if (isNaN(precioBolsa) && precioBolsa > 0) {
-      precioBolsa = parseInt(prompt("Incorrecto, ingrese el precio del producto en gondola mayor a '0'."))
+    alert("Genial, prosiga oprimiendo 'ACEPTAR' e ingresando la cantidad.");
+
+    /* CANTIDAD INGRESADA---------------------------------------------------------------------*/
+    cantidad = parseInt(prompt("Ingrese la cantidad asignada del producto que desea llevar."))
+    while (isNaN(cantidad) || cantidad <= 0) {
+      cantidad = parseInt(prompt("Por favor, ingrese un tipo de valor numerico que no sea 0 o negativo."))
     }
 
-    switch (cantidadBolsa) {
-      case tipoPro:
-        if (banderaInicioCantidad == true || tipoPro == 'arena') {
-          cantidadBolsa = cantidadBolsa + cantidadPro;
-          banderaInicioCantidad == false;
+    alert("Genial, prosiga oprimiendo 'ACEPTAR' y a continuacion se le dara un informe de su compra.")
+
+    /* PAGOS/DESCUENTOS--------------------------------------------------------------------*/
+    precioFinal = cantidad * precio;
+
+    switch (banderaCuentaPagar == true) {
+      case (cantidad > 10 && cantidad < 30):
+        precioConDescuento = precioFinal - (precioFinal * primerDescuento);
+        console.log("El importe total a pagar (con descuento del 15%) es: $" + precioConDescuento.toFixed(2));
+        break;
+      case (cantidad > 30):
+        precioConDescuento = precioFinal - (precioFinal * segundoDescuento);
+        console.log("El importe total a pagar (con descuento del 25%) es: $" + precioConDescuento.toFixed(2));
+        break;
+      default:
+        console.log("El importe total a pagar (bruto) es: $" + precioFinal.toFixed(2));
+        break;
+    }
+
+    /* PRECIO MAS CARO--------------------------------------------------------------------*/
+
+    switch (tipoBolsa) {
+      case 'A':
+        cantBolsaArena = cantBolsaArena + cantidad;
+        if (banderaPrecio == true || precio > precioArena) {
+          precioArena = precioArena + precio;
+          banderaPrecio = false;
         }
         break;
-      case tipoPro:
-        if (banderaInicioCantidad == true || tipoPro == 'cal') {
-          cantidadBolsa = cantidadBolsa + cantidadPro;
-          banderaInicioCantidad == false;
+      case 'B':
+        cantBolsaCal = cantBolsaCal + cantidad;
+        if (banderaPrecio == true || precio > precioCal) {
+          precioCal = precioCal + precio;
+          banderaPrecio = false;
         }
         break;
-      case tipoPro:
-        if (banderaInicioCantidad == true || tipoPro == 'cemento') {
-          cantidadBolsa = cantidadBolsa + cantidadPro;
-          banderaInicioCantidad == false;
+      case 'C':
+        cantBolsaCemento = cantBolsaCemento + cantidad;
+        if (banderaPrecio == true || precio > precioCemento) {
+          precioCemento = precioCemento + precio;
+          banderaPrecio = false;
         }
         break;
     }
-  }//FIN WHILE
-}//FIN FUNCTION
+
+    /* CANTIDAD ------------------------------------------------------------------------------------- */
+
+    if (cantBolsaArena > cantBolsaCal && cantBolsaArena > cantBolsaCemento) {
+      console.log("La cantidad de bolsas de Arena es mayor, son: " + cantBolsaArena.toFixed(2));
+    }
+    else if (cantBolsaCal > cantBolsaArena && cantBolsaCal > cantBolsaCemento) {
+      console.log("La cantidad de bolsas de Cal es mayor, son: " + cantBolsaCal.toFixed(2));
+    }
+    else {
+      console.log("La cantidad de bolsas de Cemento es mayor, son: " + cantBolsaCemento.toFixed(2));
+    }
+
+    /* TIPO MAS CARO COMPARACION---------------------------------------------------------------*/
+
+    if (precioArena >= precioCal && precioArena >= precioCemento) {
+      console.log("Las bolsas mas caras son las de arena, y valen: " + precioArena.toFixed(2));
+    }
+    else if (precioCal >= precioArena && precioCal >= precioCemento) {
+      console.log("Las bolsas mas caras son las de Cal, y valen: " + precioArena.toFixed(2));
+    }
+    else if (precioCemento >= precioArena && precioCemento >= precioCal) {
+      console.log("Las bolsas mas caras son las de Cemento, y valen: " + precioArena.toFixed(2));
+    }
+
+    // DESEA CONTINUAR -------------------------------------------------------------------------------
+    respuesta = prompt("¿Desea ingresar más bolsas? (si/no)").toLowerCase();
+
+    while (respuesta !== "si" && respuesta !== "no") {
+      respuesta = prompt("La respuesta ingresada no es válida. ¿Desea ingresar más bolsas? (si/no)");
+    }
+    if (respuesta == "si") {
+      banderaInicializacion = true;
+    } else if (respuesta == "no") {
+      break;
+    }
+  }
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
--- 9-Parciales/2020_1Cuatri_recu_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_recu_fuentes/tres.js
@@ -1,4 +1,101 @@
-function mostrar()
-{
-	alert("tres");
+//  Quimey Alejo Fontan
+
+// Bienvenidos.
+// En el ingreso a un viaje en avión nos solicitan nombre, nacionalidad , edad, sexo("f" o "m") y estado civil("soltero", "casado" o "viudo")y temperatura corporal.
+// a) la Nacionalidad de la persona con mas temperatura.
+// b) Cuantos mayores de edad( más de 17) estan solteros
+// c) La cantidad de Mujeres que hay solteras o viudas.
+// d) cuantas personas de la tercera edad( mas de 60 años) , tienen mas de 38 de temperatura
+// e) El promedio de edad entre las mujeres casadas.
+
+function mostrar() {
+	let nombrePersona;
+	let estadoCivil;
+	let edadPersona;
+	let sexoPersona;
+	let nacionalidadPersona;
+	let temperaturaCorporal;
+	let ingresoDeDatos;
+	let contadorSolteros;
+	let contadorSolteras;
+	let acumuladorMujeres;
+	let contadorMujeresViudas;
+	let mayoresDe60;
+	let contMayoresViudos;
+	let temperaturaMasAlta;
+	let nacionalidadTemperaturaMasAlta;
+	let hombresPromedio;
+
+	contadorSolteras = 0;
+	contadorSolteros = 0;
+	acumuladorMujeres = 0;
+	contadorMujeresViudas = 0;
+	mayoresDe60 = 0;
+	contMayoresViudos = 0;
+	ingresoDeDatos = "si";
+
+	//---------------------------------------------------------------------------------------
+
+	while (ingresoDeDatos == "si") {
+		nombrePersona = prompt("Ingrese su nombre")
+		edadPersona = parseInt(prompt("Ingrese su edad"));
+		while (edadPersona < 1 || edadPersona > 100) {
+			edadPersona = parseInt(prompt("Ingrese su edad dentro de los valores esperados (0-100"));
+		}
+
+		temperaturaCorporal = parseInt(prompt("Ingrese su temperaturaCorporal"));
+		while (isNan(temperaturaCorporal) || temperaturaCorporal < 35 || temperaturaCorporal > 45) {
+			temperaturaCorporal = parseInt(prompt("Ingrese su temperaturaCorporal"));
+		}
+
+		sexoPersona = prompt("Ingrese su sexo");
+		while (sexoPersona != "f" && sexoPersona != "m") {
+			sexoPersona = prompt("Ingrese su sexo");
+		}
+
+		nacionalidadPersona = prompt("Ingrese su nacionalidad");
+		while (!isNaN(nacionalidadPersona)) {
+			nacionalidadPersona = prompt("Ingrese su nacionalidad");
+		}
+
+		estadoCivil = prompt("Ingrese su estadoCivil");
+		while (estadoCivil != "soltero" && estadoCivil != "casado" && estadoCivil != "viudo") {
+			estadoCivil = prompt("Ingrese su estadoCivil");
+		}
+
+		if (temperaturaCorporal > temperaturaMasAlta) {
+			temperaturaMasAlta = temperaturaCorporal
+			nacionalidadTemperaturaMasAlta = nacionalidadPersona
+		}
+
+		if (edadPersona > 17 && estadoCivil == "soltero") {
+			contadorSolteros = contadorSolteros + 1
+		}
+
+		if (sexoPersona == "f") {
+			switch (estadoCivil) {
+				case 'soltero':
+					contadorSolteras = contadorSolteras + 1;
+					acumuladorMujeres = acumuladorMujeres + 1;
+					break;
+				case 'viudo':
+					contadorMujeresViudas = contadorMujeresViudas + 1;
+					break;
+			}
+		}
+
+		if (edadPersona > 59 && temperaturaCorporal > 38) {
+			mayoresDe60 = mayoresDe60 + 1
+		}
+
+		ingresoDeDatos = prompt("Quiere seguir ingresando datos?: ")
+	}
+
+	hombresPromedio = acumuladorMujeres / contadorSolteros
+
+	alert("La persona con mayor temperatura es de nacionalidad " + nacionalidadTemperaturaMasAlta)
+	alert("hay " + contadorSolteras + " mujeres solteras.")
+	alert("hay " + contadorMujeresViudas + " personas que son mujeres viudas.")
+	alert("hay " + mayoresDe60 + " mayores de 60 años con mas de 38 de temperatura")
+	alert("El promedio de edad entre hombres solteros es de " + hombresPromedio)
 }
